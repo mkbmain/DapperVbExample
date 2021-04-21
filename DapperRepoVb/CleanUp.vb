f0bc1a39a7836a0007a87274dcb3da6a903f8f97
michael.bourke@live.co.uk <michael.bourke@live.co.uk>
2021-04-21 21:48:46 +0100

    first commit

---
+++ DapperRepoVb/CleanUp.vb
@@ -0,0 +1,31 @@
+﻿
+
+' this just to clear the db and not really part of the example 
+Imports Mkb.DapperRepo.Repo
+
+Module CleanUp
+    Private Sub DeleteAllOfType(Of T)(repo As SqlRepo)
+        Dim items = repo.GetAll(Of T)()
+        For Each item As T In items
+            repo.Delete(item)
+        Next
+    End Sub
+    Private Async Function DeleteAllOfTypeAsync(Of T)(repo As SqlRepoAsync) As Task
+        Dim items = Await repo.GetAll(Of T)()
+        For Each item As T In items
+            Await repo.Delete(item)
+        Next
+    End Function
+
+    Public Async Function ClearDbAsync(repo As SqlRepoAsync) As Task
+        Await CleanUp.DeleteAllOfTypeAsync(Of Post)(repo)
+        Await CleanUp.DeleteAllOfTypeAsync(Of User)(repo)
+    End Function
+
+
+    Public Sub ClearDb(repo As SqlRepo)
+        CleanUp.DeleteAllOfType(Of Post)(repo)
+        CleanUp.DeleteAllOfType(Of User)(repo)
+    End Sub
+
+End Module
---
+++ DapperRepoVb/Program.vb
@@ -0,0 +1,84 @@
+Imports Mkb.DapperRepo.Repo
+
+
+module Program
+    Const Connection As String = "Server=192.168.0.204;Database=DapperTestDb;User Id=sa;Password=12345678;" ' you might need to change this depending on where your sql instance is and if its windows auth
+
+    Public Sub Main()
+        Task.Run(AddressOf AsyncRun).Wait() ' this is in every example but the .wait locks don't it ???? surely this is bad 
+        Run()
+    End Sub
+
+    Private Sub Run() ' sync
+        Dim repo = New SqlRepo(Connection)
+        ClearDb(repo)
+
+
+        ' we add a user
+        Dim user = repo.Add(New User With {.CreatedAt = DateTime.Now, .Email = "test@email.com", .Name = "Michael"})
+        repo.Add(New User With {.CreatedAt = DateTime.Now, .Email = "test2@email.com", .Name = ""})
+        repo.Add(New User With {.CreatedAt = DateTime.Now, .Email = "test3@email.com", .Name = "John"})
+        repo.Add(New User With {.CreatedAt = DateTime.Now, .Email = "test4@email.com", .Name = "Jane"})
+        ' we add a post
+        For i = 0 To 22
+            repo.Add(New Post With {.PostedAt = DateTime.Now, .UserId = user.Id, .Text = $"Test{i}Post"})
+        Next
+
+        ' we search for 11 post
+        Dim elven = (repo.Search(Of Post)(NameOf(Post.Text), "%11%")).ToArray()
+        If elven.Count() <> 1 Then
+            Throw New Exception("Opps")
+        End If
+        ' we edit the post
+        elven.First().Text = "Edited"
+        repo.Update(elven.First())
+
+        ' we get all posts again from db
+        Dim allPosts = repo.GetAll(Of Post)
+
+        ' we check edit happened
+        If (allPosts.Any(Function(t) t.Text = "Edited") = False) Then
+            Throw New Exception("Opps")
+        End If
+
+        ClearDb(repo)
+    End Sub
+
+    Private Async Function AsyncRun() As Task ' async
+
+        Dim repo = New SqlRepoAsync(Connection)
+        Await ClearDbAsync(repo)
+
+
+        ' we add a user
+        Dim user = Await repo.Add(New User With {.CreatedAt = DateTime.Now, .Email = "test@email.com", .Name = "Michael"})
+        Await repo.Add(New User With {.CreatedAt = DateTime.Now, .Email = "test2@email.com", .Name = ""})
+        Await repo.Add(New User With {.CreatedAt = DateTime.Now, .Email = "test3@email.com", .Name = "John"})
+        Await repo.Add(New User With {.CreatedAt = DateTime.Now, .Email = "test4@email.com", .Name = "Jane"})
+        ' we add a post
+        For i = 0 To 22
+            Await repo.Add(New Post With {.PostedAt = DateTime.Now, .UserId = user.Id, .Text = $"Test{i}Post"})
+        Next
+
+        ' we search for 11 post
+        Dim elven = (Await repo.Search(Of Post)(NameOf(Post.Text), "%11%")).ToArray()
+        If elven.Count() <> 1 Then
+            Throw New Exception("Opps")
+        End If
+        ' we edit the post
+        elven.First().Text = "Edited"
+        Await repo.Update(elven.First())
+
+        ' we get all posts again from db
+        Dim allPosts = Await repo.GetAll(Of Post)
+
+        ' we check edit happened
+        If (allPosts.Any(Function(t) t.Text = "Edited") = False) Then
+            Throw New Exception("Opps")
+        End If
+
+        Await ClearDbAsync(repo)
+    End Function
+End Module
+
+
