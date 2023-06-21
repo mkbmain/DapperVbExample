13856b3a4f72960fb32fa4bc4b4e348b4272c496
mkbmain <michael.bourke@live.co.uk>
2023-06-21 01:16:23 +0100

    name more random

--- Tests/BaseDbTestClass.vb
+++ Tests/BaseDbTestClass.vb
@@ -7,7 +7,7 @@ Imports Mkb.DapperRepo.Repo
 
 Namespace Tests
     Public Class BaseDbTestClass
-        Protected ReadOnly _dbName As String = Guid.NewGuid.ToString().Substring(0, 8) + ".db"
+        Protected ReadOnly _dbName As String = Guid.NewGuid.ToString() + ".db"
         Protected ReadOnly Connection As DbConnection = New SqliteConnection("Data Source="+_dbName)
         Protected ReadOnly Repo As SqlRepoAsync = New SqlRepoAsync(Function() Connection)
 
