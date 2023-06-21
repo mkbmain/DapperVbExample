5201d57a1ec8dfde69414ea3233347fdcbeb0d96
mkbmain <michael.bourke@live.co.uk>
2023-06-21 01:37:44 +0100

    Update Tests

--- Tests/Tests/AddTests.vb
+++ Tests/Tests/AddTests.vb
@@ -1,24 +1,22 @@
-Imports System
-Imports Dapper
 Imports DapperRepoVb.DbItems
+Imports Tests.Tests
 Imports Xunit
+Imports Dapper
 
-Namespace Tests
-    Public Class AddTests
-        Inherits BaseDbTestClass
+Public Class AddTests
+    Inherits BaseDbTestClass
 
-        <Fact>
-        Async Function EnsureWeCanAdd() As Task
-            ' how we add
-            Dim user = New User With {.Email = "test", .Name = "Mike", .CreatedAt = DateTime.Now}
-            Await Repo.Add(user)
+    <Fact>
+    Async Function EnsureWeCanAdd() As Task
+        ' how we add
+        Dim user = New User With {.Email = "test", .Name = "Mike", .CreatedAt = DateTime.Now}
+        Await Repo.Add(user)
 
-            ' asserts
-            Dim result As User = Await Connection.QueryFirstAsync(Of User)(New CommandDefinition("select * from users"))
-            Assert.Equal(user.Email, result.Email)
-            Assert.Equal(user.Name, result.Name)
-            Assert.Equal(user.CreatedAt, result.CreatedAt)
-        End Function
-    End Class
-End Namespace
+        ' asserts
+        Dim result As User = Await Connection.QueryFirstAsync(Of User)(New CommandDefinition("select * from users"))
+        Assert.Equal(user.Email, result.Email)
+        Assert.Equal(user.Name, result.Name)
+        Assert.Equal(user.CreatedAt, result.CreatedAt)
+    End Function
+End Class
 
---
+++ Tests/Tests/UpdateTests.vb
@@ -0,0 +1,28 @@
+﻿Imports DapperRepoVb.DbItems
+Imports Tests.Tests
+Imports Xunit
+Imports Dapper
+
+Public Class UpdateTests
+    Inherits BaseDbTestClass
+
+    <Fact>
+    Async Function EnsureWeCanUpdate() As Task
+        ' how we addfewfgwergerte
+        dim name as String = "Mike"
+        Await _
+            Connection.ExecuteAsync(
+                new CommandDefinition($"insert into users (Name,CreatedAt,Email) values ('{name}','2020-01-01','test')"))
+        Dim result As User = Await Connection.QueryFirstAsync (Of User)(New CommandDefinition("select * from users"))
+        Assert.NotNull(result)
+        Assert.Equal(name, result.Name)
+        result.Name = name + "2"
+
+
+        Await Repo.Update(result)
+
+        ' asserts
+        result = Await Connection.QueryFirstAsync (Of User)(New CommandDefinition("select * from users"))
+        Assert.Equal(name + "2", result.Name)
+    End Function
+End Class
