e87e3f4cef5109909d87e8e3939c19cdfd071836
mkbmain <michael.bourke@live.co.uk>
2023-06-24 09:22:31 +0100

    another one

--- Tests/Tests/GetTests.vb
+++ Tests/Tests/GetTests.vb
@@ -31,6 +31,20 @@ Public Class GetTests
     End Function
 
     <Fact>
+    Async Function EnsureWeCanSearchCount() As Task
+
+        Await AddUser()
+        Await AddUser()
+        Await AddUser("john")
+        Await AddUser()
+        ' get
+        Dim usersFromDb = Await Repo.SearchCount(New User With {.Name = "john"}, SearchCriteria.Create(NameOf(User.Name), SearchType.Equals))
+
+        ' asserts
+        Assert.Equal(1, usersFromDb)
+    End Function
+
+    <Fact>
     Async Function EnsureWeCanGetAll() As Task
         Dim user As User = Await AddUser()
         Await AddUser()
