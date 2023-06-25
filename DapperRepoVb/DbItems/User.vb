184a24ec1e906719159e5efff67c9b2e9af63780
mkbmain <michael.bourke@live.co.uk>
2023-06-25 21:37:57 +0100

    User refactor

--- DapperRepoVb/DbItems/User.vb
+++ DapperRepoVb/DbItems/User.vb
@@ -4,10 +4,10 @@ Namespace DbItems
     <SqlTableName("Users")>                             ' for pluralization but can also be used for old schema names i.e USER_TABLE is a horrible class name but may be a legacy table name
     Public Class User
         <PrimaryKey()>                                  ' used to denote this is primary key for lookups updates etc
-        Public Property Id() As Nullable(Of Integer)       ' marked as nullable as db auto insert so we don't need to, repo won't insert null fields
+        Public Property Id() As Integer?
         Public Property Name() As String
         Public Property Email() As String
-        Public Property CreatedAt() As DateTime
+        Public Property CreatedAt() As Date
         <SqlIgnoreColumn()>
         Public Property UniqueId() As String = Email + Name
     End Class
