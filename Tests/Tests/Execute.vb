d4a2897b4626b17c4b85d2a177b50ef07057bd65
mkbmain <michael.bourke@live.co.uk>
2023-06-26 05:34:28 +0100

    to fix

---
+++ Tests/Tests/Execute.vb
@@ -0,0 +1,25 @@
+Imports Dapper
+Imports Tests.Tests
+Imports Xunit
+
+Public Class ExecuteTests
+	Inherits BaseDbTestClass
+
+	<Fact>
+	Async Function EnsureWeCanDelete() As Task
+		Await AddUser()
+		Const TableName = "boohoo"
+		Dim User = Await GetFirstUser()
+		Assert.NotNull(User)
+		' delete
+
+
+		Dim tables = Connection.Execute("SELECT name FROM sqlite_master  WHERE type='" + TableName + "'")
+		Assert.Equal(-1, tables)
+		Await Repo.Execute("CREATE TABLE  " + TableName + " (id   integer constraint posts_pk primary key autoincrement)")
+
+		' asserts
+		tables = Connection.Execute("SELECT name FROM sqlite_master  WHERE type='" + TableName + "'")
+		Assert.Equal(1, tables)
+	End Function
+End Class
