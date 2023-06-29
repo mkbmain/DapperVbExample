97ea46194b19452c2054e1e2a3a3355edbb3ddf9
mkbmain <michael.bourke@live.co.uk>
2023-06-29 13:22:02 +0100

    fe

--- Tests/Tests/Execute.vb
+++ Tests/Tests/Execute.vb
@@ -12,12 +12,10 @@ Public Class ExecuteTests
         Dim User = Await GetFirstUser()
         Assert.NotNull(User)
         ' delete
-
-
+        
         Dim tablesNum = Connection.Query(of String)("SELECT name FROM sqlite_master")
         Assert.True(1 <tablesNum.Count())
-        Await _
-            Repo.Execute("CREATE TABLE  " + TableName + " (id   integer constraint posts_pk primary key autoincrement)")
+        Await Repo.Execute("CREATE TABLE  " + TableName + " (id   integer constraint posts_pk primary key autoincrement)")
 
         ' asserts
         Dim tables =connection.Query(of String)("SELECT name FROM sqlite_master")
