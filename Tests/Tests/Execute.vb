b1191dd881c61d63220dbe759da27d7ed6bef678
mkbmain <michael.bourke@live.co.uk>
2023-12-12 06:48:36 +0000

    slight optimisation

--- Tests/Tests/Execute.vb
+++ Tests/Tests/Execute.vb
@@ -10,19 +10,19 @@ Public Class ExecuteTests
     Async Function EnsureWeCanExecute() As Task
         Await AddUser()
         Const TableName = "boohoo"
-        
+
         ' execute
-        Dim tablesNum = GetTables(Connection).Count()
+        Dim tablesNum = GetTables(Connection)
         Assert.True(1 < tablesNum)
         Await _
             Repo.Execute("CREATE TABLE  " + TableName + " (id   integer constraint posts_pk primary key autoincrement)")
 
         ' asserts
-        dim newTables = GetTables(Connection).Count()
+        Dim newTables = GetTables(Connection)
         Assert.True(newTables > tablesNum)
     End Function
 
-    Private Function GetTables(connection as DbConnection) As IEnumerable(Of String)
-        Return connection.Query (of String)("SELECT name FROM sqlite_master")
+    Private Function GetTables(connection As DbConnection) As Integer
+        Return connection.ExecuteScalar(Of Integer)("SELECT count(name) FROM sqlite_master")
     End Function
 End Class
